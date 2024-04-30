9a343988a7a311ebe7cfa4adfbbbff1f1fa4a8e2
i0ne <i0ne10patch@gmail.com>
2024-04-30 13:57:06 +0300

    Rename functions

--- src/kovsh.h
+++ src/kovsh.h
@@ -67,25 +67,25 @@ typedef struct {
     ContextEntry *cursor;
 } ContextIter;
 
-Lexer lexer_new(StrSlice ss);
+Lexer ksh_lexer_new(StrSlice ss);
 
-const char *lexer_token_type_to_string(TokenType token_type);
-void lexer_inc(Lexer *l, size_t inc);
+const char *ksh_lexer_token_type_to_string(TokenType token_type);
+void ksh_lexer_inc(Lexer *l, size_t inc);
 
-Token lexer_token_next_expect(Lexer *l, TokenType expect);
-Token lexer_token_next(Lexer *l);
-bool  lexer_token_next_is(Lexer *l, TokenType t);
+Token ksh_lexer_expect_next_token(Lexer *l, TokenType expect);
+Token ksh_lexer_token_next(Lexer *l);
+bool  ksh_lexer_is_token_next(Lexer *l, TokenType t);
 
-TokenSeq lexer_token_seq_from_lexer(Lexer *l, size_t count);
+TokenSeq ksh_token_seq_from_lexer(Lexer *l, size_t count);
 
-void lexer_parse_to_context(Lexer *l, Context *context);
-void lexer_context_write(Context *context, ContextEntry *ce);
-void lexer_context_delete(Context *context);
-Context lexer_context_new(void);
+void ksh_lexer_parse_to_context(Lexer *l, Context *context);
+void ksh_context_write(Context *context, ContextEntry *ce);
+void ksh_context_delete(Context *context);
+Context ksh_context_new(void);
 
-ContextEntry *lexer_context_entry_create(TokenSeq ts, ContextUse ctx_use);
-const char *lexer_context_use_to_str(ContextUse ctx_use);
+ContextEntry *ksh_context_entry_create(TokenSeq ts, ContextUse ctx_use);
+const char *ksh_context_use_to_str(ContextUse ctx_use);
 
-bool lexer_context_iter_next(ContextIter *ctx_iter, ContextEntry **output);
+bool ksh_context_iter_next(ContextIter *ctx_iter, ContextEntry **output);
 
 #endif
