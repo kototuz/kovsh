847562e19d3d81727eefa9cb5db8d15e67dcbf64
i0ne <i0ne10patch@gmail.com>
2024-06-02 18:37:14 +0300

    Add `strv_from_str()`

--- src/common.c
+++ src/common.c
@@ -19,6 +19,11 @@ StrView strv_new(const char *data, size_t data_len)
     return (StrView){ .items = data, .len = data_len };
 }
 
+StrView strv_from_str(const char *data)
+{
+    return (StrView){ .items = data, .len = strlen(data) };
+}
+
 bool strv_eq(StrView sv1, StrView sv2)
 {
     return sv1.len == sv2.len &&
