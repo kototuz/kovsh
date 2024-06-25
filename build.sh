46011f1383e910679093f346aff48732ed4b5594
i0ne <ivann1812@gmail.com>
2024-06-25 17:44:40 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,35 +5,8 @@ mkdir -p out/bin
 
 cc=gcc
 cflags="-Wall -Wextra -Wpedantic -g"
-main=src/main.c
-sources=src/*.c
-obj_dir=out/objs
-bin_dir=out/bin
-examples=examples/*.c
+src=src/*
 
-function build_obj {
-    if [ $# -eq 1 ]; then
-        src_rep=src/
-        obj=$1
-        obj=${obj/.c/.o}
-        obj=$obj_dir/${obj/$src_rep/}
-        $cc $cflags -o $obj -c $1
-        echo "$cc $cflags -o $obj -c $1"
-    fi
-}
-
-if [ $# -eq 1 ]; then
-    case $1 in
-        src/*.c)
-            build_obj $1
-            ;;
-        "clean")
-            rm -rf $obj_dir/* $bin_dir/*
-            ;;
-    esac
-else
-    for s in $sources
-    do
-        build_obj $s
-    done
-fi
+for i in $src; do
+    $cc $cflags -o ${i/.c/.o} -c $i
+done
