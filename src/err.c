82dee2ca723d5479cb7b220f3868ce88d59a6b79
i0ne <ivann1812@gmail.com>
2024-07-05 17:19:56 +0300

    Update the error map

--- src/err.c
+++ src/err.c
@@ -3,6 +3,9 @@ static const char *errmap[] = {
     [KSH_ERR_OK] = "ok",
     [KSH_ERR_COMMAND_NOT_FOUND] = "command not found",
     [KSH_ERR_TOKEN_EXPECTED] = "token was expected",
+    [KSH_ERR_ARG_NOT_FOUND] = "arg not found",
+    [KSH_ERR_VALUE_EXPECTED] = "value was expected",
+    [KSH_ERR_PARSING_FAILED] = "parsing was failed"
 };
 
 
