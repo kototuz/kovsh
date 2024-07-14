ecef75b7790be51f0c84fb905fdb8ebaf4e1df60
i0ne <ivann1812@gmail.com>
2024-07-15 01:31:24 +0300

    Add example with usage of `ksh_parse_cargs()`

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@
 cc=gcc
 cflags="-Wall -Wextra -Wpedantic -g"
 src=kovsh.c
-test=examples/simpleterm.c
+term=examples/simpleterm.c
+parse_cargs=examples/cargs.c
 
 if [[ $1 == "test" ]]; then
-     $cc $cflags -o test.out $test $src
+    $cc $cflags -o term.out $test $src
+elif [[ $1 == "parse_cargs" ]]; then
+    $cc $cflags -o cargs.out $parse_cargs $src
 else $cc $cflags -o test.o -c $src
 fi
---
+++ examples/cargs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "../kovsh.h"
+
+int main(int argc, char **argv)
+{
+    KshParser p = {0};
+
+    StrView msg;
+    KSH_PARAMS(&p, KSH_STORE(msg, "message"));
+    if (!ksh_parse_cargs(&p, argc, argv)) return 1;
+
+    printf(STRV_FMT"\n", STRV_ARG(msg));
+
+    return 0;
+}
