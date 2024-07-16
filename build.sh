e2094e3ff46aa741b61fef1eb4b34006c3d17ab3
i0ne <ivann1812@gmail.com>
2024-07-16 18:15:26 +0300

    Update `build.sh`

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ src=kovsh.c
 term=examples/simpleterm.c
 parse_cargs=examples/cargs.c
 
-if [[ $1 == "test" ]]; then
-    $cc $cflags -o term.out $test $src
+if [[ $1 == "term" ]]; then
+    $cc $cflags -o term.out $term $src
 elif [[ $1 == "cargs" ]]; then
     $cc $cflags -o cargs.out $parse_cargs $src
 else $cc $cflags -o test.o -c $src
