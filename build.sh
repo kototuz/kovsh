eca6ff413ce07cc61aeeb85a86525a3e523ec206
i0ne <ivann1812@gmail.com>
2024-07-27 00:33:36 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ parse_cargs=examples/cargs.c
 
 if [[ $1 == "term" ]]; then
     $cc $cflags -o term.out $term $src
+    ./term.out
 elif [[ $1 == "cargs" ]]; then
     $cc $cflags -o cargs.out $parse_cargs $src
 else $cc $cflags -o test.o -c $src
