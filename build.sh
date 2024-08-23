132bc8ee104921922060aea1e72764f152b539f5
i0ne <ivann1812@gmail.com>
2024-08-23 14:41:10 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 cc=gcc
 cflags="-Wall -Wextra -Wpedantic -g"
@@ -6,10 +6,11 @@ src=kovsh.c
 term=examples/simpleterm.c
 parse_cargs=examples/cargs.c
 
-if [[ $1 == "term" ]]; then
-    $cc $cflags -o term.out $term $src
+if [ "$1" = "term" ]; then
+    $cc $cflags -o term.out examples/simpleterm.c $src
     ./term.out
-elif [[ $1 == "cargs" ]]; then
-    $cc $cflags -o cargs.out $parse_cargs $src
-else $cc $cflags -o test.o -c $src
+elif [ "$1" = "cargs" ]; then
+    $cc $cflags -o cargs.out examples/cargs.c $src
+else
+    $cc $cflags -o test.o -c $src
 fi
